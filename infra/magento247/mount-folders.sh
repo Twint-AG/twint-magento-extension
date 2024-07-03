897422ed1419cc3a9f3d5a7f40f545f41bb420c8
Sang Nguyen Thanh <sang.nguyenthanh@nfq.com>
2024-07-03 09:12:04 +0000

    Update mount folders

---
+++ infra/magento247/mount-folders.sh
@@ -0,0 +1,11 @@
+mount_dir=/data/app
+app_dir=/var/www/html
+while IFS= read -r folder
+do
+    mkdir -p $mount_dir/$folder
+    mkdir -p $app_dir/$folder
+    cp -af $app_dir/$folder/. $mount_dir/$folder
+    rm -rf $app_dir/$folder
+    ln -sf $mount_dir/$folder $app_dir/$folder
+    chown -R www-data:www-data $app_dir/$folder
+done < $app_dir/mount-folders.txt
