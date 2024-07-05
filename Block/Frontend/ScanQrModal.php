4cf6d0f26b8b4a9e203e5d1abe704c40230fc4cc
Tai Tran <tai@kosmonaut.io>
2024-07-05 22:15:54 +0700

    Revert "Use for styling"
    This reverts commit eccc114a1e42450de31e2146db4e285a886e339e.

--- Block/Frontend/ScanQrModal.php
+++ Block/Frontend/ScanQrModal.php
@@ -38,8 +38,8 @@ class ScanQrModal extends Template
         $html = '';
 
         if ($isAndroid) {
-            $html .= '<div class="!text-center !mt-4 !px-4">
-                        <a id="twint-addroid-button" class="!block !mb-1 !w-full !bg-black !text-white !font-bold !p-4 !rounded-lg !text-center hover:bg-gray-800 focus:outline-none focus:ring-gray-600 focus:ring-opacity-75
+            $html .= '<div class="text-center mt-4 px-4">
+                        <a id="twint-addroid-button" class="block mb-1 w-full bg-black text-white font-bold p-4 rounded-lg text-center hover:bg-gray-800 focus:outline-none focus:ring-gray-600 focus:ring-opacity-75
                             hover:text-white hover:no-underline
                         "
                            href="javascript:window.location = ' . $links['android'] . '">
@@ -65,7 +65,7 @@ class ScanQrModal extends Template
                 $icon = $prefinedApps[$link['name']] ?? null;
                 if ($icon) {
                     $app .= '<img src="' . $this->getViewFileUrl("Twint_Magento/images/apps/{$icon}.png") . '" 
-                    class="!shadow-2xl !w-64 !h-64 !rounded-3xl !mx-auto"
+                    class="shadow-2xl w-64 h-64 rounded-3xl mx-auto"
                     data-link="' . $link['link'] . '"
                     alt="' . $link['name'] . '">';
                 } else {
@@ -75,15 +75,15 @@ class ScanQrModal extends Template
 
             $html .= '
                 <div id="twint-ios-container">
-                    <div class="!my-6 !text-center">
+                    <div class="my-6 text-center">
                         ' . __('Select your TWINT app:') . '
                     </div>
         
-                    <div class="twint-app-container !w-3/4 !mx-auto !justify-center !max-w-screen-md !mx-auto !grid !grid-cols-3 !gap-4">
+                    <div class="twint-app-container w-3/4 mx-auto justify-center max-w-screen-md mx-auto grid grid-cols-3 gap-4">
                         ' . $app . '
                     </div>
                     
-                    <select class="!h-55 !block !my-4 !w-full !p-4 !bg-white !text-center !appearance-none !border-none focus:outline-none focus:ring-0">
+                    <select class="h-55 block my-4 w-full p-4 bg-white text-center appearance-none border-none focus:outline-none focus:ring-0">
                         <option>' . __('Other banks') . '</option>
                         ' . $else . '
                     </select>    
@@ -93,15 +93,15 @@ class ScanQrModal extends Template
 
         if ($isIos || $isAndroid) {
             $html .= '
-                <div class="!text-center !md:hidden">
-                    <div class="!flex !items-center !justify-center !my-4">
-                        <div class="!flex-grow !border-t !border-gray-300"></div>
-                        <span class="!mx-4 !text-black">' . __('or') . '</span>
-                        <div class="!flex-grow !border-t !border-gray-300"></div>
+                <div class="text-center md:hidden">
+                    <div class="flex items-center justify-center my-4">
+                        <div class="flex-grow border-t border-gray-300"></div>
+                        <span class="mx-4 text-black">' . __('or') . '</span>
+                        <div class="flex-grow border-t border-gray-300"></div>
                     </div>
     
-                    <div class="!qr-code !my-3">
-                        <div class="!text-center">' . __('Enter this code in your TWINT app:') . '</div>
+                    <div class="row qr-code my-3">
+                        <div class="col-9 text-center">' . __('Enter this code in your TWINT app:') . '</div>
                     </div>
                 </div>
             ';
