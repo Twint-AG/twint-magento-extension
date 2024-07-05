07c0d01c9fcda37d6a4758d2a794fa44eeb99045
Tai Tran <tai@kosmonaut.io>
2024-07-05 21:38:51 +0700

    update build

--- infra/magento247/run.sh
+++ infra/magento247/run.sh
@@ -1,7 +1,1 @@
-rm -rf /var/www/html/vendor/twint/magento-2/zinfra
 
-bin/magento setup:upgrade
-bin/magento setup:di:compile
-bin/magento setup:static-content:deploy -f
-bin/magento cache:clean
-bin/magento cache:flush
