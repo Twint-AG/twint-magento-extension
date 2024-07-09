f866d54fb8dafa6cb290e88f073433f4ad6b546e
sangnguyennfq <sang.nguyenthanh@nfq.com>
2024-07-09 15:16:24 +0700

    Add infra for magento 2.4.4

---
+++ infra/magento244/update_module.sh
@@ -0,0 +1,20 @@
+rm -rf /var/www/html/vendor/twint/magento-2
+composer update twint/magento-2 --no-progress --no-interaction
+
+# Avoid di compile issue when running setup:di:compile
+rm -rf /var/www/html/vendor/twint/magento-2/zinfra
+
+bin/magento module:enable Twint_Magento
+
+# Update Database schema and classes prototype
+bin/magento setup:upgrade
+
+# Build Dependency injection objects
+bin/magento setup:di:compile
+
+# Build client resources (JS, CSS ...)
+bin/magento setup:static-content:deploy -f
+
+# Clear cache again
+bin/magento cache:clean
+bin/magento cache:flush
