9d5bd1d7ce13d5785c87e162f6020ae5d2fdfcc0
Tai Tran <tai@kosmonaut.io>
2024-07-22 14:28:16 +0700

    Update styling for product box

--- Block/Frontend/Express/Screen/Category/Button.php
+++ Block/Frontend/Express/Screen/Category/Button.php
@@ -1,8 +1,31 @@
 <?php
+
 namespace Twint\Magento\Block\Frontend\Express\Screen\Category;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Block\Product\AwareInterface as ProductAwareInterface;
+use Magento\Catalog\Model\Product;
 use Twint\Magento\Block\Frontend\Express\Button as Base;
 use Twint\Magento\Constant\TwintConstant;
 
-class Button extends Base{
+class Button extends Base implements ProductAwareInterface
+{
+    protected ProductInterface $product;
     const SCREEN = TwintConstant::SCREEN_PLP;
+
+    public function setProduct(ProductInterface $product)
+    {
+        $this->product = $product;
+    }
+
+    protected function shouldRender(): bool
+    {
+        $should = parent::shouldRender();
+
+        if ($this->product instanceof Product) {
+            return $should && $this->product->isSaleable();
+        }
+
+        return $should;
+    }
 }
