63b2a2343a990dd4ffca9350ee34cfb042773afb
Tai Tran <tai@kosmonaut.io>
2024-08-05 12:09:29 +0700

    fix request log link

--- Block/Adminhtml/Listing/LogAction.php
+++ Block/Adminhtml/Listing/LogAction.php
@@ -6,19 +6,20 @@ namespace Twint\Magento\Block\Adminhtml\Listing;
 
 use Magento\Sales\Ui\Component\Listing\Column\ViewAction as MagentoViewAction;
 
-class ViewAction extends MagentoViewAction
+class LogAction extends MagentoViewAction
 {
     public function prepareDataSource(array $dataSource): array
     {
         if (isset($dataSource['data']['items'])) {
             $viewUrlPath = $this->getData('config/viewUrlPath') ?: '#';
             $label = __($this->getData('config/text') ?: 'View');
+            $field = $this->getData('config/field') ?: 'id';
 
             foreach ($dataSource['data']['items'] as &$item) {
                 $item[$this->getData('name')] = [
                     'view' => [
                         'href' => $this->urlBuilder->getUrl($viewUrlPath, [
-                            'id' => $item['id'],
+                            'id' => $item[$field],
                         ]),
                         'label' => $label,
                     ],
